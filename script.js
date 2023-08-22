b9800c3444a3d59abb5f77843417302ddd46592f
Felipe Jeldres <142822343+FelipeJeldres@users.noreply.github.com>
2023-08-21 21:33:18 -0400

    Update script.js
    aproximacion sin decimales

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function calcularTotalHerramientas() {
 
 // Función para mostrar el resultado en la página
 function mostrarResultado() {
-    const totalMecanica = Math.round(calcularTotalMecanica()*1.1);
+    const totalMecanica = calcularTotalMecanica()*1.1.toFixed(0);
     const totalHerramientas = calcularTotalHerramientas();
     const totalGeneral = totalMecanica + totalHerramientas;
 
